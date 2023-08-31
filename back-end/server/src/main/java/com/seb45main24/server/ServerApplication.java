e3a6b5192a2c3ff6ab72acc38b8e4cacbee46be4
hojun lee <ghwns123212@gmail.com>
2023-08-31 10:32:46 +0900

    [init]project setting

---
+++ back-end/server/src/main/java/com/seb45main24/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.seb45main24.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
