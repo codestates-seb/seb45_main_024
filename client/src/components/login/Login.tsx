9cb9fbe3ed08a984b36dea3eeadffaa630b67147
kimD0ngjun <chickentasty0112@gmail.com>
2023-09-03 18:28:16 +0900

    [feat] 로그인 기능 #14 박스 디자인

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,7 +1,30 @@
 import { FC } from "react";
+import classes from "./Login.module.css";
 
 const Login: FC = () => {
-  return <div>Login</div>;
+  return (
+    <div className={classes.container}>
+      <form className={classes.signUp}>
+        <div className={classes.inputInfo}>
+          <label>Nickname</label>
+          <input placeholder="Input Nickname" type="text" />
+          <p>닉네임은 2글자 이상 7글자 이하여야 합니다</p>
+        </div>
+        <div className={classes.inputInfo}>
+          <label>Email</label>
+          <input placeholder="Input Email" type="text" />
+          <p>유효한 이메일 형식이 아닙니다</p>
+        </div>
+        <div className={classes.inputInfo}>
+          <label>Password</label>
+          <input placeholder="Input Password" type="password" />
+          <p>비밀번호는 5글자 이상이어야 합니다</p>
+        </div>
+        <div className={classes.inputInfo}></div>
+        <button>Sign Up</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- client/src/pages/login/LoginPage.tsx
+++ client/src/pages/login/LoginPage.tsx
@@ -1,9 +1,13 @@
 import { FC } from "react";
+import classes from "./LoginPage.module.css";
 import Login from "../../components/login/Login";
+import SocialLogin from "../../components/login/SocialLogin";
 
 const LoginPage: FC = () => {
   return (
-    <div>
+    <div className={classes.container}>
+      <img alt="logo"></img>
+      <SocialLogin />
       <Login />
     </div>
   );
