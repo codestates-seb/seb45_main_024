1fcd8e42154875321e7e5117e0699d00f3d26e51
kimD0ngjun <chickentasty0112@gmail.com>
2023-09-03 19:21:28 +0900

    [feat] 로 그인 기능 #14 박스 디자인 보완

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -6,22 +6,21 @@ const Login: FC = () => {
     <div className={classes.container}>
       <form className={classes.signUp}>
         <div className={classes.inputInfo}>
-          <label>Nickname</label>
-          <input placeholder="Input Nickname" type="text" />
-          <p>닉네임은 2글자 이상 7글자 이하여야 합니다</p>
-        </div>
-        <div className={classes.inputInfo}>
           <label>Email</label>
           <input placeholder="Input Email" type="text" />
-          <p>유효한 이메일 형식이 아닙니다</p>
+          <p className={classes.errorMessage}>유효한 이메일 형식이 아닙니다</p>
         </div>
         <div className={classes.inputInfo}>
           <label>Password</label>
           <input placeholder="Input Password" type="password" />
-          <p>비밀번호는 5글자 이상이어야 합니다</p>
+          <div className={classes.flexContainer}>
+            <p className={classes.errorMessage}>
+              비밀번호는 5글자 이상이어야 합니다
+            </p>
+            <p className={classes.forgotPassword}>Forgot Password?</p>
+          </div>
         </div>
-        <div className={classes.inputInfo}></div>
-        <button>Sign Up</button>
+        <button>Log In</button>
       </form>
     </div>
   );
