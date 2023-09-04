c902a836d0a481728536dbc618aa8a02c4e47d6d
agnes0304 <jiwoochoi0304@gmail.com>
2023-09-04 17:56:11 +0900

    [feat] EditMyInfo ongoing

--- client/src/components/mypage/EditMyInfo.tsx
+++ client/src/components/mypage/EditMyInfo.tsx
@@ -8,7 +8,7 @@ const EditMyInfo: FC = () => {
       <div className={classes.infoHeaderBox}>
         <h1 className={classes.headerTitle}>나의 정보</h1>
         <p className={classes.helptext}>회원가입 시에 입력하신 정보입니다.</p>
-        <p className={classes.helptext}>
+        <p className={`${classes.helptext} ${classes.warning}`}>
           개인 정보 보호를 위해서 비밀번호 재발급 시에 비밀번호를 변경해주세요.
         </p>
       </div>
@@ -16,19 +16,27 @@ const EditMyInfo: FC = () => {
         <h2 className={classes.contentTitle}>개인정보</h2>
         <div className={classes.infoContent}>
           <div className={classes.myImg}>(프로필 이미지)</div>
-          <p className={classes.userNickname}>닉네임</p>
-          <p className={classes.myNickname}>(유저 이름)</p>
-          <p className={classes.userEmail}>가입 이메일</p>
-          <p className={classes.myEmail}>(유저 이메일)</p>
-          <p className={classes.userPw}>비밀번호</p>
-          <p className={classes.myPw}>
-            (유저 비밀번호 앞에 4개만 보이고 나머진 *로 표시)
-          </p>
+          <div className={classes.infoitems}>
+            <div className={classes.infoitem}>
+              <p className={classes.fieldname}>닉네임</p>
+              <p className={classes.myNickname}>(유저 이름)</p>
+            </div>
+            <div className={classes.infoitem}>
+              <p className={classes.fieldname}>가입 이메일</p>
+              <p className={classes.myEmail}>(유저 이메일)</p>
+            </div>
+            <div className={classes.infoitem}>
+              <p className={classes.fieldname}>비밀번호</p>
+              <p className={classes.myPw}>
+                (유저 비밀번호 앞에 4개만 보이고 나머진 *로 표시)
+              </p>
+            </div>
+          </div>
         </div>
-        <div className={classes.profileBox}>
-          <h2 className={classes.contentTitle}>프로필</h2>
-          <button>프로필 수정하기</button>
-        </div>
+      </div>
+      <div className={classes.profileBox}>
+        <h2 className={classes.contentTitle}>프로필</h2>
+        <button>프로필 수정하기</button>
       </div>
     </>
   );
--- client/src/components/mypage/TechTags.tsx
+++ client/src/components/mypage/TechTags.tsx
@@ -15,7 +15,7 @@ const sample = [
 
 const TechTags: FC = () => {
   // 개수 제한: 받아오는건 다 받아오고, 10개까지만 렌더링
-  // 더보기 버튼
+  // 더보기 버튼 클릭시 나머지 렌더링
   // onClick: techName을 TechStack에 추가해서 위로 올리기(CreateProfile에서 post 요청)
 
   return (
