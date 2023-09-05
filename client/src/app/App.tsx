e8784ac1b993182f5b6db8fe78c3f29df58509f1
sowon_m1 <bsw2377@gmail.com>
2023-09-05 23:10:29 +0900

    [feat] Route 처리 수정

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,26 +1,103 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
-/* 임시 라우팅 처리 */
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import RootLayout from "../pages/Root";
+import ErrorPage from "../pages/Error";
+
 import Main from "../pages/main/Main";
+
+// import Mypage from "../pages/mypage/Mypage";
+
 import UserList from "../pages/userList/UserList";
-// import ComponentName from "../pages/";
-// import ComponentName from "../pages/";
+import NewCard from "../pages/userList/NewCard";
+import EditCard from "../pages/userList/EditCard";
+
+import ProjectList from "../pages/projectList/ProjectList";
+import Detail from "../pages/projectList/Detail";
+import NewPost from "../pages/projectList/NewPost";
+import EditPost from "../pages/projectList/EditPost";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Main /> },
+
+      /*** 📌 로그인/회원가입 ***/
+      {
+        path: "/",
+        children: [
+          {
+            path: "login",
+            // element: <Login />,
+            // children: [{ path: "findpassword", element: <PAGE /> }],
+          },
+          {
+            path: "signup",
+            // element: <Signup />,
+          },
+        ],
+      },
+
+      /*** 📌 마이페이지 ***/
+      {
+        path: "mypage",
+        // element: <Mypage />,
+        /*
+        children: [
+          {
+            path: "profile",
+            // element: <PAGE />,
+            children: [
+              {
+                path: ":id",
+                // element: <PAGE />,
+              },
+              {
+                path: "edit",
+                // element: <PAGE />,
+              },
+            ],
+          },
+          {
+            path: "summary",
+            // element: <PAGE />,
+          },
+          {
+            path: "review",
+            // element: <PAGE />,
+          },
+        ],
+        */
+      },
+
+      /*** 📌 팀찾기 ***/
+      {
+        path: "userlist",
+        children: [
+          { index: true, element: <UserList /> },
+          { path: "new", element: <NewCard /> },
+          { path: "edit/:id", element: <EditCard /> },
+        ],
+      },
+
+      /*** 📌 팀원찾기 ***/
+      {
+        path: "projectlist",
+        children: [
+          { index: true, element: <ProjectList /> },
+          { path: ":id", element: <Detail /> },
+          { path: "new", element: <NewPost /> },
+          { path: "edit/:id", element: <EditPost /> },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<Main />}></Route>
-            <Route path="/userlist" element={<UserList />}></Route>
-            {/* <Route path="/" element={<ComponentName />}></Route>  */}
-            {/* <Route path="/" element={<ComponentName />}></Route>  */}
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ client/src/pages/Error.tsx
@@ -0,0 +1,11 @@
+const ErrorPage = () => {
+  return (
+    <main>
+      <h1 style={{ fontSize: "3rem" }}>ERROR PAGE</h1>
+      <p>유효하지 않은 경로입니다.</p>
+      <p>임시 에러페이지 입니다. 수정해서 작업 부탁드립니다 😊</p>
+    </main>
+  );
+};
+
+export default ErrorPage;
