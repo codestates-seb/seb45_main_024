639bd850e79597fe4d9fea0b80d5298b1d8d07e8
agnes0304 <jiwoochoi0304@gmail.com>
2023-09-05 14:37:43 +0900

    [feat] 회원정보 컴포넌트

--- client/src/components/mypage/EditMyInfo.tsx
+++ client/src/components/mypage/EditMyInfo.tsx
@@ -5,39 +5,56 @@ const EditMyInfo: FC = () => {
   return (
     <>
       {/* 프로필 이미지, 닉네임, 패스워드, 프로필정보 변경할 수 있는 버튼, 회원탈퇴 버튼 */}
-      <div className={classes.infoHeaderBox}>
+      <section className={classes.infoHeaderBox}>
         <h1 className={classes.headerTitle}>나의 정보</h1>
         <p className={classes.helptext}>회원가입 시에 입력하신 정보입니다.</p>
         <p className={`${classes.helptext} ${classes.warning}`}>
           개인 정보 보호를 위해서 비밀번호 재발급 시에 비밀번호를 변경해주세요.
         </p>
-      </div>
-      <div className={classes.infoContentBox}>
-        <h2 className={classes.contentTitle}>개인정보</h2>
+      </section>
+      <section className={classes.infoContentBox}>
+        <div className={classes.infoContentHeader}>
+          <h2 className={classes.contentTitle}>회원정보</h2>
+          <button className={classes.editInfo}>회원정보 변경</button>
+        </div>
         <div className={classes.infoContent}>
-          <div className={classes.myImg}>(프로필 이미지)</div>
+          <div className={classes.profileImgContainer}>
+            <img
+              className={classes.profileimage}
+              src="https://images.unsplash.com/photo-1682687980976-fec0915c6177?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2670&q=80"
+              alt="sampleProfile"
+            />
+          </div>
           <div className={classes.infoitems}>
             <div className={classes.infoitem}>
               <p className={classes.fieldname}>닉네임</p>
-              <p className={classes.myNickname}>(유저 이름)</p>
+              <p className={classes.fieldvalue}>(유저 이름)</p>
             </div>
             <div className={classes.infoitem}>
               <p className={classes.fieldname}>가입 이메일</p>
-              <p className={classes.myEmail}>(유저 이메일)</p>
+              <p className={classes.fieldvalue}>(유저 이메일)</p>
             </div>
             <div className={classes.infoitem}>
               <p className={classes.fieldname}>비밀번호</p>
-              <p className={classes.myPw}>
-                (유저 비밀번호 앞에 4개만 보이고 나머진 *로 표시)
+              <p className={`${classes.fieldvalue} ${classes.password}`}>
+                (samplepassword)
               </p>
             </div>
           </div>
         </div>
-      </div>
-      <div className={classes.profileBox}>
+      </section>
+      <section className={classes.profileBox}>
         <h2 className={classes.contentTitle}>프로필</h2>
-        <button>프로필 수정하기</button>
-      </div>
+        <div className={classes.profileContainer}>
+          <div className={classes.profileContent}>
+            {/* 여기 컴포넌트 일부 렌더링 */}
+          </div>
+          <div className={classes.overlay}>
+            <button className={classes.editProfile}>프로필 수정하기</button>
+            {/* 라우팅 변경 */}
+          </div>
+        </div>
+      </section>
     </>
   );
 };
