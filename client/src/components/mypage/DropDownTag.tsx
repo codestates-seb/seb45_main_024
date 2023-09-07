6e6f7276005275ec367e008906786ec0bcd2e1a9
agnes0304 <jiwoochoi0304@gmail.com>
2023-09-08 01:31:26 +0900

    [feat]dropdown logic

--- client/src/components/mypage/DropDownTag.tsx
+++ client/src/components/mypage/DropDownTag.tsx
@@ -1,5 +1,25 @@
-import { FC, useState } from "react";
+import { FC, useState, useEffect, useRef } from "react";
 import classes from "./DropDownTag.module.css";
+
+export const ArrowDownSvg = () => {
+  return (
+    <svg
+      width="14"
+      height="8"
+      viewBox="0 0 14 8"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1 1L7 7L13 1"
+        stroke="var(--color-gray-3)"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
 
 interface DropDownTagProps {
   techName: string;
@@ -9,23 +29,49 @@ interface DropDownTagProps {
 
 const DropDownTag: FC<DropDownTagProps> = ({ techName, id, onDelete }) => {
   const [selectedLevel, setSelectedLevel] = useState("A");
+  const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+  
+  // 드롭다운 외 부분 클릭 시 드롭다운 닫힘
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    const handleClickOutside = (e: any) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        setIsDropDownOpen(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [dropdownRef]);
 
   return (
-    <div className={classes.dropDowntagContainer}>
+    <div className={classes.dropDownTagContainer} ref={dropdownRef}>
       <span className={classes.tagName} onClick={() => onDelete(id)}>
         {techName}
       </span>
-      <select
+      <div
         className={classes.levelDropdown}
-        value={selectedLevel}
-        onChange={e => setSelectedLevel(e.target.value)}
+        onClick={() => setIsDropDownOpen(!isDropDownOpen)}
       >
-        <option value="A">A</option>
-        <option value="B">B</option>
-        <option value="C">C</option>
-        <option value="D">D</option>
-        <option value="E">E</option>
-      </select>
+        <ArrowDownSvg />
+        <div
+          className={`${classes.optionsContainer} ${
+            isDropDownOpen ? "" : classes.hidden
+          }`}
+        >
+          {["A", "B", "C", "D", "E"].map((level, index) => (
+            <div
+              key={index}
+              className={classes.option}
+              onClick={() => setSelectedLevel(level)}
+            >
+              {level}
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
