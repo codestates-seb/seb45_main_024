68241bbfedc7692ab8d31c95ad02d68f19b64664
hojun lee <ghwns123212@gmail.com>
2023-09-07 13:54:08 +0900

    [fix] 팀찾기 entity 수정

--- back-end/server/src/main/java/com/seb45main24/server/domain/teamboard/entity/TeamBoard.java
+++ back-end/server/src/main/java/com/seb45main24/server/domain/teamboard/entity/TeamBoard.java
@@ -20,7 +20,7 @@ import java.util.List;
 @Table(name = "team_boards")
 public class TeamBoard extends Auditable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long teamBoardId;
 
     @Column(nullable = false)
