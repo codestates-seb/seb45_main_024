e17de736259c38d0ff6179b9f3efc091eef1ec1d
sowon_m1 <bsw2377@gmail.com>
2023-09-12 14:37:49 +0900

    test

--- client/src/redux/board/thunks/addUserCard.ts
+++ client/src/redux/board/thunks/addUserCard.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import authInstance from "../../utility/authInstance";
+import authInstance from "../../../utility/authInstance";
 
 interface dataType {
   title: string;
