c65091e80add621f7cd65dc407c5708b1860a10c
jiwon <sahel526@gmail.com>
2023-09-14 18:01:53 +0900

    [feat] 팀보드게시판 닉네임 추가 #12

--- back-end/server/src/main/java/com/seb45main24/server/domain/teamboard/repository/TeamBoardRepository.java
+++ back-end/server/src/main/java/com/seb45main24/server/domain/teamboard/repository/TeamBoardRepository.java
@@ -9,5 +9,4 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TeamBoardRepository extends JpaRepository<TeamBoard, Long> {
 	List<TeamBoard> findTeamBoardsByAccountId(Long accountId);
-
 }
