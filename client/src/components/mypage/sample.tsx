d9dce04bf2bc5cf9cf9f8b8f5b780e17dc6fda26
agnes0304 <jiwoochoi0304@gmail.com>
2023-09-18 14:43:05 +0900

    [feat] drag and drop test

--- client/src/components/mypage/sample.tsx
+++ client/src/components/mypage/sample.tsx
@@ -1,6 +1,6 @@
-import { FC, useState } from "react";
+import { FC, useState, useRef, useEffect } from "react";
 import classes from "./sample.module.css";
-import default_profile from "../../assets/images/default_profile.svg";
+import logoCircle from "../../assets/images/logo-circle.svg";
 
 type Logo = {
   id: number;
@@ -9,69 +9,79 @@ type Logo = {
 };
 
 const Sample: FC = () => {
-  //   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
-  const [dragged, setDragged] = useState<Logo | null>(null);
-  const [dropped, setDropped] = useState<{
-    logo: Logo;
-    x: number;
-    y: number;
-  } | null>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [pos, setPos] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+  const [originPos, setOriginPos] = useState<{ x: number; y: number }>({
+    x: 0,
+    y: 0,
+  });
 
-  const handleDragStart = (e: React.DragEvent<HTMLImageElement>, item: any) => {
-    setDragged(item);
+  useEffect(() => {
+    const updatePosition = () => {
+      const rect = containerRef.current?.getBoundingClientRect();
+      const imgWidth = 100;
+      const imgHeight = 100;
+      if (rect) {
+        setPos({
+          x: (rect.width - imgWidth) / 2,
+          y: (rect.height - imgHeight) / 2,
+        });
+      }
+    };
+
+    window.addEventListener("resize", updatePosition);
+
+    updatePosition();
+
+    return () => {
+      window.removeEventListener("resize", updatePosition);
+    };
+  }, []);
+
+  const handleDragStart = (e: React.DragEvent<HTMLImageElement>) => {
+    setOriginPos({ x: e.clientX - pos.x, y: e.clientY - pos.y });
   };
 
-  const handleDrop = (e: React.DragEvent<HTMLDivElement>) => {
-    e.preventDefault();
-    if (dragged) {
-      setDropped({ logo: dragged, x: e.clientX, y: e.clientY });
-    }
+  const handleDrag = (e: React.DragEvent<HTMLImageElement>) => {
+    if (e.clientX === 0 && e.clientY === 0) return;
+    setPos({ x: e.clientX - originPos.x, y: e.clientY - originPos.y });
   };
 
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
   };
-  const logo = { id: 1, name: "logo1", image: default_profile };
+
+  const logo: Logo = { id: 1, name: "logo1", image: logoCircle };
 
   return (
-    <div className={classes.containerBox}>
-      <div
-        className={`${classes.container} ${classes.first}`}
-        onDrop={handleDrop}
-        onDragOver={handleDragOver}
-      >
-        {dropped === null && (
-          <img
-            key={logo.id}
-            src={logo.image}
-            alt={logo.name}
-            draggable="true"
-            onDragStart={(e) => handleDragStart(e, logo)}
-            className={classes.logo}
-          />
-        )}
+    <>
+      <div className={classes.back}>
+        <img
+          src={logo.image}
+          draggable
+          alt={logo.name}
+          onDragStart={handleDragStart}
+          onDrag={handleDrag}
+          onDragOver={handleDragOver}
+          style={{
+            left: `${pos.x}px`,
+            top: `${pos.y}px`,
+            position: "absolute",
+          }}
+        />
+        <div className={`${classes.background}`} ref={containerRef}></div>
+        <div className={`${classes.background}`} ref={containerRef}>
+          <section className={`${classes.box} ${classes.findTeam}`}>
+            <h1>Find your team!</h1>
+            <p>중앙의 로고를 움직여 보세요</p>
+          </section>
+          <section className={`${classes.box} ${classes.findMate}`}>
+            <h1>Find your mate!</h1>
+            <p>중앙의 로고를 움직여 보세요!</p>
+          </section>
+        </div>
       </div>
-      <div
-        className={`${classes.container} ${classes.second}`}
-        onDrop={handleDrop}
-        onDragOver={handleDragOver}
-      >
-        {dropped && (
-          <img
-            key={logo.id}
-            src={logo.image}
-            alt={logo.name}
-            draggable="true"
-            style={{
-              top: `${dropped.y}px` + 200,
-              left: `${dropped.x}px` + 200,
-            }}
-            onDragStart={(e) => handleDragStart(e, logo)}
-            className={classes.logo}
-          />
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 
