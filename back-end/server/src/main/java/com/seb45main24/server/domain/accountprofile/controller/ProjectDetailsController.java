b9e382dcea79ac82a791c04a773ec8bc9fd7af23
dotollll <k44790488@gmail.com>
2023-09-20 16:41:28 +0900

    [fix] 프로필 이미지 수정, 프로젝트 이미지 등록

--- back-end/server/src/main/java/com/seb45main24/server/domain/accountprofile/controller/ProjectDetailsController.java
+++ back-end/server/src/main/java/com/seb45main24/server/domain/accountprofile/controller/ProjectDetailsController.java
@@ -8,11 +8,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.seb45main24.server.domain.accountprofile.dto.ProjectDetailRequest;
 import com.seb45main24.server.domain.accountprofile.dto.ProjectDetailResponse;
 import com.seb45main24.server.domain.accountprofile.entity.ProjectDetails;
+import com.seb45main24.server.domain.accountprofile.mapper.ProjectDetailsMapper;
 import com.seb45main24.server.domain.accountprofile.service.ProjectDetailService;
 import com.seb45main24.server.global.argumentresolver.LoginAccountId;
 
@@ -24,18 +27,18 @@ import lombok.RequiredArgsConstructor;
 public class ProjectDetailsController {
 
 	private final ProjectDetailService projectDetailService;
+	private final ProjectDetailsMapper projectDetailsMapper;
 
 	@PostMapping("/projectDetails")
-	public ResponseEntity postProjectDetails(@LoginAccountId Long loginAccountId, @RequestBody ProjectDetailRequest request) {
+	public ResponseEntity postProjectDetails(@LoginAccountId Long loginAccountId,
+												@RequestPart(required = false) MultipartFile multipartFile,
+												ProjectDetailRequest request) {
 
-		ProjectDetails content = projectDetailService.createProjectDetails(loginAccountId, request);
+		ProjectDetails projectDetails = projectDetailsMapper.toProjectDetail(request);
 
-		ProjectDetailResponse response = new ProjectDetailResponse();
-			response.setProjectDetailId(content.getId());
-			response.setProjectTitle(content.getProjectTitle());
-			response.setProjectUrl(content.getProjectUrl());
-			response.setImageUrl(content.getImage().getImageUrl());
-			response.setAccountProfileId(content.getAccountProfile().getId());
+		ProjectDetails content = projectDetailService.createProjectDetails(loginAccountId, projectDetails, multipartFile);
+
+		ProjectDetailResponse response = projectDetailsMapper.toProjectDetailResponse(content);
 
 		return new ResponseEntity(response, HttpStatus.OK);
 	}
